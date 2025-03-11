c29ec583c88399bcc9a6cdf53e8d34dbdf177979
biwott-v <victor.biwott@student.moringaschool.com>
2025-03-11 08:30:16 +0300

    completes assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+  let messagearray=[]
+  for (let name of names) {
+    messagearray.push(`Thank you, ${name}, for the wonderful ${event} gift!`);
+  }
+  return messagearray;
+}
+
+function countDown(number) {
+  while (number >= 0) {
+    console.log(number);
+    number -= 1;
+  }
+}
